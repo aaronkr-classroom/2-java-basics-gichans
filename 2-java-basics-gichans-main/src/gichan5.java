d33b102d534e2b8c69058fe2fd96f8bcfe52fd11
gichans <162276953+gichans@users.noreply.github.com>
2024-03-12 17:33:25 +0900

    Add files via upload

---
+++ 2-java-basics-gichans-main/src/gichan5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class gichan5 {
+
+	public static void main(String[] args) {
+	
+	Scanner s = new Scanner(System.in);
+	
+	System.out.println("Type your name: ");
+	String name = s.nextLine();
+	System.out.println("Welcome, " + name + "!");
+	
+	System.out.println("Type your age: ");
+	int age = s.nextInt();
+	System.out.println("Your age: " + age);
+}
+}
---
+++ 2-java-basics-gichans-main/src/gichans.java
@@ -0,0 +1,9 @@
+
+public class gichans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
